ea060c801e5228a04ae5e072231dda421731fca8
xuesongc <jchen@goldencomm.com>
2019-11-26 00:28:49 -0800

    inital console screen commit

--- src/Views/ConsoleScreen.js
+++ src/Views/ConsoleScreen.js
@@ -4,6 +4,9 @@ import '../style/ConsoleScreen.css'
 const ConsoleScreen = (props) => {
     return (
         <div className={'ConsoleScreen'}>
+            <div className="title">
+                <h1>Tic-Tac-Remix!</h1>
+            </div>
         </div>
     )
 }
